9545c1a9a413855a148e2a85f7486e58bafaeb4c
gvantsa kapanadze <gvantsakapanadze95@gmail.com>
2023-04-18 20:19:41 +0400

    last

--- index.ts
+++ index.ts
@@ -1,18 +1,21 @@
-const password = document.getElementById("password")as HTMLInputElement; //textcontent
-let copied = document.getElementById("copied")as HTMLInputElement; //none
+const password = document.getElementById("password")as HTMLInputElement;
+let copied = document.getElementById("copied")as HTMLInputElement; 
 let icon = document.getElementById("icon")as HTMLInputElement;
 let ten = document.getElementById("ten")as HTMLSpanElement;
 let rangeInput = document.getElementById("range")as HTMLInputElement;
-let uppercase = document.getElementById("Uppercase")as HTMLInputElement;//input
+let uppercase = document.getElementById("Uppercase")as HTMLInputElement;
 let lowercase = document.getElementById("Lowercase")as HTMLInputElement;
 let numbers = document.getElementById("numbers")as HTMLInputElement;
 let synbols = document.getElementById("Synbols")as HTMLInputElement;
-let color_div = document.getElementById("color_div")as HTMLInputElement;//array and for each
+let color_div1 = document.getElementById("color_div1")as HTMLInputElement;
+let color_div2 = document.getElementById("color_div2")as HTMLInputElement;
+let color_div3 = document.getElementById("color_div3")as HTMLInputElement;
+let color_div4 = document.getElementById("color_div4")as HTMLInputElement;
 let button = document.getElementById("butt")as HTMLInputElement;
-let medium = document.getElementById("medium")as HTMLInputElement;//textcontent
+let medium = document.getElementById("medium")as HTMLInputElement;
 let input_up = document.getElementById("input_up")as HTMLInputElement;
-let slidervalue: number = 10;
 
+let slidervalue: number = 10;
 function change( event: Event) {
     const inputElement = event.target as HTMLInputElement;
     const thumbPosition: number = (parseInt(inputElement.value) - parseInt(inputElement.min)) / (parseInt(inputElement.max) - parseInt(inputElement.min)) * 100;
@@ -21,45 +24,89 @@ function change( event: Event) {
     const slidervalue = parseInt(inputElement.value);
     ten.textContent = slidervalue.toString();
   };
+  //for update new valueo of input
   rangeInput.addEventListener("input", change);
+
   button.addEventListener("click", ()=>{ 
-    const checkboxes = document.querySelectorAll<HTMLInputElement>('input[type="checkbox"]');//length4
+    const checkboxes = document.querySelectorAll<HTMLInputElement>('input[type="checkbox"]');
     let result = '';
     let score = 0;
       if(uppercase.checked){
         result += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        score += 2;
+        score += 3;
       };
       if(lowercase.checked){
         result += "abcdefghijklmnopqrstuvwxyz"
-        score += 4;
+        score += 5;
       }
       if(numbers.checked){
         result += "0123456789";
-        score += 6;
+        score += 4;
       }
       if(synbols.checked){
         result += "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-        score += 8;
-      }
-      score = Math.ceil(score /5);
-      
-      console.log(score);
-      debugger
-      if(score === 1 ){
-        console.log("hello")
+        score += 4;
       }
       const myString = ten.textContent;
       const myNumber = Number(myString);
       let output: string = "";
-      console.log(myNumber);
       for (let i = 0; i < myNumber; i++){
         const caracters =  getRandomCharacter(result);
-        output += caracters //concatenation
+        output += caracters
       }
       function getRandomCharacter(str: string): string {
           const randomIndex = Math.floor(Math.random() * result.length);
           return str.charAt(randomIndex);
         } 
+      password.textContent = output;
+
+      score = Math.ceil(score /5);
+      if(score === 1 ){
+        color_div1.style.background= "#F64A4A"
+        color_div2.style.background= "#18171F"
+        color_div3.style.background= "#18171F"
+        color_div4.style.background= "#18171F"
+        medium.textContent= "TOO WEAK!";
+      }
+      if(score === 2 ){
+        color_div1.style.background= "#FB7C58"
+        color_div2.style.background= "#FB7C58"
+        color_div3.style.background= "#18171F"
+        color_div4.style.background= "#18171F"
+        medium.textContent= "WEAK";
+      }
+      if(score === 3 ){
+        color_div1.style.background= "#F8CD65"
+        color_div2.style.background= "#F8CD65"
+        color_div3.style.background= "#F8CD65"
+        color_div4.style.background= "#18171F"
+        medium.textContent= "MEDIUM";
+      }
+      if(score ===4 ){
+        color_div1.style.background= "#A4FFAF"
+        color_div2.style.background= "#A4FFAF"
+        color_div3.style.background= "#A4FFAF"
+        color_div4.style.background= "#A4FFAF"
+        medium.textContent= "STRONG";
+      }
+      if(score === 0) {
+        color_div1.style.background= "#18171F"
+        color_div2.style.background= "#18171F"
+        color_div3.style.background= "#18171F"
+        color_div4.style.background= "#18171F"
+        medium.style.display ="none";
+        copied.style.display= "none";
+      }
+      
     });
- 
+
+    function copy<T>(item: T): T {
+        return JSON.parse(JSON.stringify(item));
+      } 
+    icon.addEventListener("click",()=> {
+      copied.style.display= "flex";
+      const copyPassword = copy(password.textContent)
+      setTimeout(() => {
+        copied.style.display = "none";
+      }, 3000); 
+    });
--- readmeforme.md
+++ readmeforme.md
@@ -1,10 +1,34 @@
-1)password.textcontent = rangeinput.length
-2)when clicked uppercase- password textcontent should be just uppercase
-3)when clicked lowercase- password textcontent should be lowercase and if there is already clicked uppercase, it should me some of them upper and some of them lowercases
-4)if clicked numbers- password textcontent should be numberss or + whatr is clicked 
-5)if we clicked symbols, textcontent should be symbols too and if there is a clicked somethins length should % clicked numbers and write password random
-6)if there is a one cicke , shoud change color of one div, if there is two, should change 2 divs color and if ther eis  clicked should change color and should change text (medium, strong weak and too weak and colors according to  it)
-7)all of them should hepaaned when we click button "generate"
+In designe :
+   i used "input[type = "range"]" method , for this slider and still standartly
+   also : input[type="range"]::-webkit-slider-thumb method and gave thisval solid border.
 
-and last :
-8)when clicked copys' button I should copy this text and display flex some text 
+   I used input type checkbox, which have min and max value and steps. Up have div , whichone styled : position absolute and "input[type="checkbox"]:checked + .input_up" where is background pic and background color. that means when checked happaned something, but in real its cheched jus input type chechbox.
+
+   for typescript: I used this video, whichone I'll used in next project too.
+    npm start < its compilation function from terminal (ts into js)>
+
+    1)the firs function is for slidervalues function, wen it move, value is changing and background changing too.
+    2)as HTMLSpanElement, .toString(), parseInt(string into integer);
+
+    after I have addeventlistener on utton and after is major code. += this is a for concatenation. when someone cklick one input, there is uppercases or lowercases or numbers or symbols and result will took this value what this input have in ts code and afetr next click add next value and if we clicked every input, where will be sum of values.
+    after we understand what is slidervalues value and math processes on it. I used this function for understand random number from Result :
+    "
+     function getRandomCharacter(str: string): string {
+          const randomIndex = Math.floor(Math.random() * result.length);
+          return str.charAt(randomIndex);
+        } "
+
+        for divs colors changing, we imagine scores system and they dividing 5 and get whole numbers and if this number (nashti) = 1,2,3,4 (t was max) i wrote code which color should be background color.
+        function : score = Math.ceil(score /5); we have Math.,floor too and Its amrgvalebs numbers + for exlample : if I have 3,7 it daamrgvalebs 4 and math.ceil daamrgvalebs 3mde and its good because whe need lower nombers cuz if it will daamrgvalebs +,, maybe after this number % 5 and we dont need it.
+
+        for copy function, I find this code in ts :
+        function copy<T>(item: T): T {
+        return JSON.parse(JSON.stringify(item));
+      } 
+      and used setTimeout function too when I wand display nnone or flex "copied" 
+      
+
+
+      :root{
+    --red:#ffffff its variable in css
+}  this is a variabe in css if I wanna use same thing multi time, I can ake this variable and after call.
